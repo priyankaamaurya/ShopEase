af40b397b2449f9deff2c9563f200b65c4ce7290
priyanka <prachii8826@gmail.com>
2026-06-02 23:45:15 +0530

    updated files

--- react_project/src/component/signup/SignUp.jsx
+++ react_project/src/component/signup/SignUp.jsx
@@ -1,4 +1,4 @@
-import Styles from "./Signup.module.css";
+import Styles from "./SignUp.module.css";
 import axios from 'axios';
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
